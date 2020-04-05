ff1a1df0be9f103bf8130fc7561b003c2e67e5a5
William George Cook <wgeorgecook@gmail.com>
2020-04-05 15:39:14 -0700

    Add the app back to index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Needs from './Needs'
 import * as serviceWorker from './serviceWorker';
-import NewNeed from "./NewNeed";
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NewNeed />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
