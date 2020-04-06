1cdffa653f6ba85a4accfc63f3aabb334e989e37
William George Cook <wgeorgecook@gmail.com>
2020-04-06 16:53:27 -0700

    Reduce some duplicated code

--- frontend/src/Needs.jsx
+++ frontend/src/Needs.jsx
@@ -4,7 +4,7 @@ import NewNeed from './NewNeed'
 import { Grid, IconButton, Dialog, DialogContent, DialogContentText } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
-const useFetch = url => {
+const useFetch = (url) => {
     const [{ data, loading }, setState] = useState({
         data: null,
         loading: true,
@@ -34,45 +34,41 @@ const newNeedReducer = (state, action) => {
 
 const Needs = () => {
     // TODO: this needs to re-render when a user accepts a need to fulfil
-    const {data, loading} = useFetch("https://meetneeds.herokuapp.com/getall?pagenumber=1");
-    const [ state, dispatch ] = useReducer(newNeedReducer, {newNeedOpen: false})
+    const { data, loading } = useFetch("https://meetneeds.herokuapp.com/getall?pagenumber=1");
+    const [ state, dispatch ] = useReducer(newNeedReducer, {newNeedOpen: false});
 
     const closeNeed = () => {
         dispatch({newNeedOpen: false})
     };
 
-    if(loading) {
-        return(
-            <div>
-                <h3>Needs Available to Meet</h3>
-                <div>loading...</div>
-            </div>
-        )
-    }
-
     return (
         <div>
             <h3>Needs Available to Meet</h3>
-            <Grid container spacing={4}>
-                {[...data].map(n => (
-                        <Grid item key={n._id}>
-                            {(n.isMet) ? null : <Need {...n}/>}
-                        </Grid>
-                ))}
-            </Grid>
-            {(!state.newNeedOpen)
-                ? <IconButton onClick={() => dispatch({type:"open"})} > <AddIcon />Submit a new need </IconButton>
-                : <Dialog open={state.newNeedOpen} onClose={closeNeed} aria-labelledby="form-dialog-title" >
-                    <DialogContent>
-                        <DialogContentText>
-                            Plase use this form to submit a new need.
-                        </DialogContentText>
-                        <NewNeed closeNeed={closeNeed}/>
-                        <DialogContentText>
-                            Refer to our Privacy Policy for privacy details.
-                        </DialogContentText>
-                    </DialogContent>
-                  </Dialog>
+            {
+                loading
+                ?  <div>loading...</div>
+                :  <Grid container spacing={4}>
+                        {[...data].map(n => (
+                                <Grid item key={n._id}>
+                                    {(n.isMet) ? null : <Need {...n}/>}
+                                </Grid>
+                        ))}
+                    </Grid>
+            }
+            {
+                !state.newNeedOpen
+                    ? <IconButton onClick={() => dispatch({type:"open"})} > <AddIcon />Submit a new need </IconButton>
+                    : <Dialog open={state.newNeedOpen} onClose={closeNeed} aria-labelledby="form-dialog-title" >
+                        <DialogContent>
+                            <DialogContentText>
+                                Plase use this form to submit a new need.
+                            </DialogContentText>
+                            <NewNeed closeNeed={closeNeed}/>
+                            <DialogContentText>
+                                Refer to our Privacy Policy for privacy details.
+                            </DialogContentText>
+                        </DialogContent>
+                    </Dialog>
             }
         </div>
     )
