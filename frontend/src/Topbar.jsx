138168a298f39fed466bb63bff39864008ac6c51
William George Cook <wgeorgecook@gmail.com>
2020-04-07 13:59:12 -0700

    Move everything to Ant Design

--- frontend/src/Topbar.jsx
+++ frontend/src/Topbar.jsx
@@ -1,6 +1,8 @@
 import React, { useReducer } from 'react'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 import NewNeed from './NewNeed'
-import { Button, Divider, Modal, PageHeader } from 'antd';
+import { Button, Divider, Form, Modal, PageHeader } from 'antd';
 
 const newNeedReducer = (state, action) => {
     switch (action.type) {
@@ -16,9 +18,61 @@ const newNeedReducer = (state, action) => {
 
 const Topbar = () => {
     const [ state, dispatch ] = useReducer(newNeedReducer, {newNeedOpen: false});
-
+    const [form] = Form.useForm();
+    const url = "https://cors-anywhere.herokuapp.com/https://meetneeds.herokuapp.com/create";
     const closeNeed = () => {
         dispatch({newNeedOpen: false})
+    };
+
+    const successfulPost = () => {
+        toast('Need successfully created', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            });
+
+        closeNeed();
+    };
+
+    // TODO: Validate entries
+    const submitData = (formData) => {
+        if (!(formData.name && formData.need && (formData.phone || formData.email))) {
+            alert("Please enter your name, your need, and at least one way we can contact you.")
+            return;
+        }
+
+        async function sendData() {
+            const resp = await axios.post(
+                url,
+                {
+                    "needingUser": {
+                        "name": formData.name,
+                        "email": formData.email,
+                        "phone": formData.phone,
+                    },
+                    "need": formData.need,
+                },
+                { "headers":
+                        {
+                            'Content-Type': 'application/json',
+                        }
+                }
+            );
+            const data = resp;
+
+            if (resp.status === 200) {
+                successfulPost();
+            } else {
+                alert("Something went wrong, please try again")
+            }
+
+            console.log("Data received: ", data)
+        }
+
+        sendData();
     };
 
 
@@ -28,9 +82,19 @@ const Topbar = () => {
             subTitle="Connecting Your Community"
             extra={<Button type="primary" onClick={() => dispatch({type:"open"})}> New need </Button>}
             >
-            <Modal visible={state.newNeedOpen} onCancel={closeNeed} onOk={closeNeed} >
+            <Modal
+                visible={state.newNeedOpen}
+                onCancel={closeNeed}
+                onOk={() => {
+                    form
+                      .validateFields()
+                      .then(values => {
+                        form.resetFields();
+                        submitData(values);
+                      })}}
+            >
                 <p>Plase use this form to submit a new need.</p>
-                <NewNeed closeNeed={closeNeed}/>
+                <NewNeed form={form} closeNeed={closeNeed}/>
                 <p>Refer to our Privacy Policy for privacy details.</p>
             </Modal>
             <Divider />
