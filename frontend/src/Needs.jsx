86a70c3ae370bbea176c515ab8866d07230ebba6
William George Cook <wgeorgecook@gmail.com>
2020-04-12 16:59:37 -0700

    Add a loader while loading

--- frontend/src/Needs.jsx
+++ frontend/src/Needs.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Topbar from './Topbar'
 import Need from './Need'
 import urls from './urls'
-import { Row, Col } from 'antd';
+import { Card, Col, Row } from 'antd';
 
 const useFetch = (url) => {
     const [{ data, loading }, setState] = useState({
@@ -17,20 +17,21 @@ const useFetch = (url) => {
         .then(data => setState({data: data, loading: false}))
     }, [url]);
 
-    return {data, loading};
+    return { data, loading };
 };
 
 
 const Needs = () => {
-    // TODO: this needs to re-render when a user accepts a need to fulfil
-    const { data, loading } = useFetch(`${urls.GET_URL}?pagenumber=1`);
+    // TODO: this needs to re-render when a user accepts a need to fulfil or submits a new need
+    const queryURL = `${urls.GET_URL}?pagenumber=1`;
+    const { data, loading } = useFetch(queryURL);
 
     return (
         <div>
             <Topbar />
             {
                 (loading)
-                ?  <div>loading...</div>
+                ?  <Card loading={loading} />
                 :  <Row gutter={[16, 16]}>
                         {[...data].map(n => (
                                 <Col key={n._id}>
